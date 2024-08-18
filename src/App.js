0561ce3436ec5721ed0cb7e2ff7a53dca797658f
Balavardhan Reddy Malyala <62328664+balavardhan224@users.noreply.github.com>
2024-08-18 14:29:30 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+ 
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map google={this.props.google} zoom={14}>
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        
+      </Map>
+    );
+  }
 }
-
-export default App;
+ 
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyD-AZ92L2sC8SInQZyfNBuo9BsE7i5GNUs')
+})(MapContainer)
